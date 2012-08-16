72c3562308692f933b9bbd4ca7a461c03617b1ab
Kyle Benson <kebenson@uci.edu>
2012-08-16 14:49:00 -0700

    Got eclipse workspace converter working better and recursively

--- eclipse-workspace-project-converter.py
+++ eclipse-workspace-project-converter.py
@@ -1,42 +1,100 @@
 #! /usr/bin/python
 
-usage=\
+ECLIPSE_WORKSPACE_PROJECT_CONVERTER_DESCRIPTION=\
 '''
-Searches each folder in this directory (or the one specified in arg1);
+Searches each assignment submission (folder) in this directory (or the one specified in arg1);
 if it is an Eclipse workspace (identified by looking for .metadata in it),
-  then move the first project in the workspace up one level to replace the current directory (but keeping the directory's name).
+then move the first project in the workspace (or the one matching the second argument) 
+up to replace the assignmennt submission directory (but keeping the directory's name).
+
+It should search recursively so that it will fix conditions where the workspace is inside
+of another folder.
 
 For example:
 
      ./eclipse-workspace-project-converter.py ~/LabExam1/possible-workspaces
 
 '''
-from os import listdir, rename, rmdir, getcwd
-from os.path import isdir, join, split
+from os import listdir, rename, rmdir, getcwd, walk
+import os.path
 from sys import argv
 from shutil import rmtree, move
+import difflib
 
-if len(argv) > 1:
-    top_dir = argv[1]
-else:
-    top_dir = '.'
+def isEclipseWorkspace(path, dirs):
+     '''
+     Check if the current directory is an eclipse workspace by looking at the version.ini
+     folder inside of the .metadata folder
+     '''
 
-for folder in listdir(top_dir):
-     folder = join(top_dir,folder)
-     if isdir(folder) and 'LabExam2' in listdir(folder):
-#'.metadata' in listdir(folder):
-        #print folder
-        #print join(top_dir,folder,'.metadata')
-        #rmtree(join(top_dir,folder,'.metadata'))
+     if '.metadata' in dirs:
+          version_file = os.path.join(path, '.metadata', 'version.ini')
+          
+          if os.path.exists(version_file):
+               with open(version_file) as f:
+                    if 'eclipse' in f.read():
+                         return True
+     
+     return False
 
-          project_name = join(folder,'LabExam2')
-          dest = split(project_name)[0]
+def getAssignmentDirectory(top_dir, path):
+     '''
+     Get the proper assignment directory that is somewhere in path given
+     that top_dir is the folder that contains all of the assignments.
+     '''
+     
+     # First get the match between the two directories
+     path = os.path.normpath(path)
+     match = difflib.SequenceMatcher(a=path, b=top_dir).get_matching_blocks()
+     match = [d for d in match if d.size and path[d.a : d.a + d.size] != os.path.sep][-1]
 
-          for i in listdir(project_name):
-            #print i
-               old = join(project_name,i)
-               
-               move(old, dest)
-            #print i, isdir(i)
-               
-          rmdir(project_name)
+     diff = path[match.a + match.size :]
+
+     # Now, pull out the first directory in, which should be the assignment since that's
+     # what we expected from the user's arguments!
+     diff = [d for d in diff.split(os.path.sep) if d][0]
+
+     return os.path.join(top_dir, diff)
+     
+def moveAssignmentUp(assignment_name, dest, path, dirs):
+     '''
+     Move the contents of the folder specified by assignment name (or the first non-hidden one if given None)
+     to the given destination folder, deleting everything else in the folder specified by path
+     (including the path itself)
+     '''
+
+     if assignment_name:
+          if assignment_name not in dirs:
+               print "Couldn't find assignment " + assignment_name + " in this Eclipse workspace!"
+               return
+          dirToMove = os.path.join(path, assignment_name)
+     else:
+          # Get first project folder
+          dirToMove = os.path.join(path, [d for d in dirs if d != '.metadata'][0])
+
+     for c in os.listdir(dirToMove):
+          move(os.path.join(dirToMove,c), os.path.join(dest, c))
+
+     rmtree(path)
+     
+if __name__ == '__main__':
+
+     if len(argv) > 1:
+          if '--help' == argv[1] or argv[1] == '-h':
+               print ECLIPSE_WORKSPACE_PROJECT_CONVERTER_DESCRIPTION
+          top_dir = argv[1]
+     else:
+         top_dir = '.'
+
+     top_dir = os.path.normpath(top_dir)
+     
+     if len(argv) > 2:
+          assignment_name = argv[2]
+     else:
+          assignment_name = None
+
+     for (path, dirs, files) in walk(top_dir):
+          if isEclipseWorkspace(path, dirs):
+               assignmentDirectory = getAssignmentDirectory(top_dir, path)
+               moveAssignmentUp(assignment_name, assignmentDirectory, path, dirs)
+               dirs = [] #stop recursing
