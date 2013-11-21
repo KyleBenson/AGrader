8fae062d65557e3745ce705f34dfa2f66516374f
Kyle Benson <kebenson@uci.edu>
2013-11-21 09:59:37 -0800

    Added a readme and fixed up some of the main files for grading

--- examples/ics51/lab3/main.cpp
+++ examples/ics51/lab3/main.cpp
@@ -1,65 +1,125 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 
 extern char* name;
 extern char* studentID;
 extern char* netID;
-extern int binarySearch();
+
+extern int binarySearch(int* arr, int arrSize, int key, int* count);
+//don't declare as extern if we want to compile without the student submission
+//int binarySearch(int* arr, int arrSize, int key, int* count);
+
+//more points for getting correct index than in the same # of iterations
+int resultPoints = 15, countPoints = 5;
+
+using namespace std;
 
 int  binarySearchInC(int* arr, int arrSize, int key, int* count)
 {
+  int result=-1;
+  int low =0;
+  int high = arrSize-1;
+  int middle;
+  *count = 0;
 
-	int result=-1;
-	int low =0;
-	int high = arrSize-1;
-	int middle;
-	*count = 0;
-
-	while(high>=low){
-		middle = (low+high)/2;
-		*count = *count + 1;
-		if(arr[middle] == key){
-			result=middle;
-			break;
-		}
-		else if(key>arr[middle])
-			low = middle + 1;
-		else
-			high = middle - 1;
-	}
-	return result;
+  while(high>=low){
+    middle = (low+high)/2;
+    *count = *count + 1;
+    if(arr[middle] == key){
+      result=middle;
+      break;
+    }
+    else if(key>arr[middle])
+      low = middle + 1;
+    else
+      high = middle - 1;
+  }
+  return result;
 }
 
-int main()
+int main(int args, char ** argv)
 {
-	//Change intArray and keyArray to vary tests
-	//Remember intArray must be sorted in ascending order with 1<=size<=100
-	int intArray[] = {1,2,3,4,5,6,7,8,9,10}; 
-	int keyArray[] = {2,1,4,11,13};
+  char* ucinetid = "NO_UCINETID";
+  if (args > 1) {
+    ucinetid = argv[1];
+  }
+
+  //Change intArray and keyArray to vary tests
+  //Remember intArray must be sorted in ascending order with 1<=size<=100
+  int intArray[] = {1,2,3,4,5,6,7,8,9,10}; 
+  int keyArray[] = {2,1,4,11,13};
 	
-	int intArraySize = sizeof(intArray)/sizeof(int);
-	int keyArraySize = sizeof(keyArray)/sizeof(int);
+  int intArraySize = sizeof(intArray)/sizeof(int);
+  int keyArraySize = sizeof(keyArray)/sizeof(int);
 
-	int count[1];
-	int i, result;
+  int expectedCount[1];
+  int theirCount[1];
+  int i;
+  int expectedResult=-1, theirResult = -1;
 
-	printf ("Name: %s\nID#: %s\nUCINetID: %s\n", name, studentID, netID);
+  int totalCorrect = 0;
+  //printf ("Name: %s\nID#: %s\nUCINetID: %s\n", name, studentID, netID);
 
-	printf("\n\nInput array:\n");
-	for(i=0;i<intArraySize;i++)		
-		printf("%d ",intArray[i]);
+  /*printf("\n\nInput array:\n");
+    for(i=0;i<intArraySize;i++)		
+    printf("%d ",intArray[i]);
 
-	printf("\n\nKeys:\n");
-	for(i=0;i<keyArraySize;i++)	
-		printf("%d ",keyArray[i]);
+    printf("\n\nKeys:\n");
+    for(i=0;i<keyArraySize;i++)	
+    printf("%d ",keyArray[i]);*/
 	
-	//Tests
-	for(i=0;i<keyArraySize;i++) {
-		*count = 0;
-		result =  binarySearchInC(intArray, intArraySize, keyArray[i], count);
-		printf("\n\nKey %d should be at position %d, and count should be %d\n", keyArray[i], result, *count);
-		*count = 0;
-		result =  binarySearch(intArray, intArraySize, keyArray[i], count);
-		printf("\nYour result: key %d is at position %d, and count equals %d\n", keyArray[i], result, *count);
-	}
+  //Tests
+  for(i=0;i<keyArraySize;i++) {
+    *expectedCount = 0;
+    expectedResult =  binarySearchInC(intArray, intArraySize, keyArray[i], expectedCount);
+    //printf("\n\nKey %d should be at position %d, and count should be %d\n", keyArray[i], result, *count);
+    //printf("%d", result);
+    *theirCount = 0;
+    theirResult =  binarySearch(intArray, intArraySize, keyArray[i], theirCount);
+    //printf("\nYour result: key %d is at position %d, and count equals %d\n", keyArray[i], result, *count);
+    
+    // score this result
+    if (expectedResult == theirResult)
+      totalCorrect+=resultPoints;
+    
+    if (*expectedCount == *theirCount)
+      totalCorrect+=countPoints;
+    // partial credit for off by one errors in # of checks
+    else if (*expectedCount == *theirCount -1 || *expectedCount == *theirCount + 1)
+      totalCorrect+=countPoints/2;
+  }
+
+  // Output results to file
+  ofstream out_file;
+  out_file.open(ucinetid);
+  out_file << ucinetid << "," << totalCorrect << endl;
+  out_file.close();
+
+  return 0;
 }
+
+// uncomment to compile without linking to student submissions
+/*int  binarySearch(int* arr, int arrSize, int key, int* count)
+{
+  int result=-1;
+  int low =0;
+  int high = arrSize-1;
+  int middle;
+  *count = 0;
+
+  while(high>=low){
+    middle = (low+high)/2;
+    *count = *count + 1;
+    if(arr[middle] == key){
+      result=middle;
+      break;
+    }
+    else if(key>arr[middle])
+      low = middle + 1;
+    else
+      high = middle - 1;
+  }
+  return result;
+}
+*/
--- examples/ics51/lab4/main.cpp
+++ examples/ics51/lab4/main.cpp
@@ -1,48 +1,78 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int pointsPerByteCorrect = 6; // gives remainder of 4 points for 4 questions, so give those for free
 
 extern char *yourName;
 extern char *studentID;
-extern int numTests;
+//extern int numTests; //we want to always do 4 tests
+
 extern void bit_operations(unsigned long inputDWord, unsigned long *outputDWord);
+//use this declaration for compiling without linking to student submission
+//void bit_operations(unsigned long inputDWord, unsigned long *outputDWord) {*outputDWord = 0x55aa033c;}
 
-void main()
+int main(int args, char ** argv)
 {
-	unsigned long myVar;
-	unsigned long tmpValue;
-    unsigned long shouldBe;
-	int i;
+  char* ucinetid = "NO_UCINETID";
+  if (args > 1) {
+    ucinetid = argv[1];
+  }
 
-	printf("NAME: %s\n\n", yourName);
+  int numTests = 4;
+  unsigned long myVar;
+  unsigned long tmpValue;
+  unsigned long shouldBe;
+  int i, totalCorrect=4; //100 isn't evenly divisible by 4 tests that are each 4 bytes, so just give them the remainder
+  
+  // Test Generator (up to four)
+  if( numTests > 4 )
+    numTests = 4;
 
-	// Test Generator (up to four)
-	if( numTests > 4 )
-		numTests = 4;
-
-    // Expected values
-	for( i = 0; i < numTests; i++){
-		switch( i ){
-			case 0: myVar = 0xaa00a1f2;
-                shouldBe = 0x55aa033c;
-				break;
-			case 1: myVar = 0x0c0c0c0c;
-                shouldBe = 0x30a602c3;
-				break;
-			case 2: myVar = 0xa0b0c0d0;
-                shouldBe = 0x051a023c;
-				break;
-			case 3: myVar = 0x80ff85f0;
-                shouldBe = 0x0155033c;
-				break;
-			default: myVar = 0xaa00a1f2;
-                shouldBe = 0x55aa033c;
-        }
-
-        printf("RUNNING TEST %d :: ", i+1);
-		printf("INPUT BYTES: %X\n\n", myVar);
-
-        tmpValue = myVar;
-
-        bit_operations(myVar, &tmpValue);
-        printf("RESULT -> SHOULD BE: %X - is: %X\n\n", shouldBe, tmpValue);
+  // Expected values
+  for( i = 0; i < numTests; i++){
+    switch( i ){
+    case 0: myVar = 0xaa00a1f2;
+      shouldBe = 0x55aa033c;
+      break;
+    case 1: myVar = 0x0c0c0c0c;
+      shouldBe = 0x30a602c3;
+      break;
+    case 2: myVar = 0xa0b0c0d0;
+      shouldBe = 0x051a023c;
+      break;
+    case 3: myVar = 0x80ff85f0;
+      shouldBe = 0x0155033c;
+      break;
+    default: myVar = 0xaa00a1f2;
+      shouldBe = 0x55aa033c;
     }
+
+    //printf("RUNNING TEST %d :: ", i+1);
+    //printf("INPUT BYTES: %X\n\n", myVar);
+
+    tmpValue = myVar;
+
+    bit_operations(myVar, &tmpValue);
+    //printf("RESULT -> SHOULD BE: %X - is: %X\n\n", shouldBe, tmpValue);
+
+    // score results, giving partial credit for getting individual bytes correct
+    if ((tmpValue & 0x000000ff) == (shouldBe & 0x000000ff))
+      totalCorrect += pointsPerByteCorrect;
+    if ((tmpValue & 0x0000ff00) == (shouldBe & 0x0000ff00))
+      totalCorrect += pointsPerByteCorrect;
+    if ((tmpValue & 0x00ff0000) == (shouldBe & 0x00ff0000))
+      totalCorrect += pointsPerByteCorrect;
+    if ((tmpValue & 0xff000000) == (shouldBe & 0xff000000))
+      totalCorrect += pointsPerByteCorrect;
+  }
+
+  // Output total correct to file named as ucinetid
+  ofstream out_file;
+  out_file.open(ucinetid);
+  out_file << ucinetid << "," << totalCorrect << endl;
+  out_file.close();
+
+  return 0;
 }
