ac62931eca78965a606639d9201e0eaf6a0e7d07
Kyle Benson <kyle.edward.benson@gmail.com>
2015-10-16 14:01:01 -0700

    made home directory use more portable

--- helper_scripts/gdata_creds.py
+++ helper_scripts/gdata_creds.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
+import os.path
+
 from oauth2client.client import flow_from_clientsecrets
-flow = flow_from_clientsecrets('/Users/kyle/Downloads/gdata_creds.json', scope='https://spreadsheets.google.com/feeds', redirect_uri='http://localhost')
+flow = flow_from_clientsecrets(os.path.join(os.path.expanduser('~'), '.gdata_creds.json')), scope='https://spreadsheets.google.com/feeds', redirect_uri='http://localhost')
 auth_uri = flow.step1_get_authorize_url()
 print auth_uri
 theCode = raw_input("Navigate to the above URL and then enter the code pulled from the redirect URL: ")
@@ -11,5 +13,5 @@ credentials = flow.step2_exchange(theCode)
 # now we save the credentials to a file for loading later into AGrader
 # NOTE: you may need to specify the credentials file via the argument --gdata_creds
 from oauth2client.file import Storage
-storage = Storage('/Users/kyle/.gdata.creds')
+storage = Storage(os.path.join(os.path.expanduser('~'), '.gdata.creds'))
 storage.put(credentials)
