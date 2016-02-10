4cdae84e9e214a2d44a5bba570e4b71cded06edf
Kyle Benson <kyle.edward.benson@gmail.com>
2016-02-10 12:36:56 -0800

    modified sid2ucid for more cleaned up roster file

--- helper_scripts/sid2ucid.py
+++ helper_scripts/sid2ucid.py
@@ -3,20 +3,30 @@
 this script reads an input roster file and converts all of the files named [student-id].txt in the submission directory
 to ones named [UCINetID].txt
 
-the roster file is not the tab-delimited one, but just the text version copy/pasted in.
+the roster file is not the tab-delimited one, but just the text version copy/pasted in with all columns
+OTHER THAN the sid and UCINetID removed (the @uci.edu part of the email address is also removed).
 the tabbed version didn't work for some reason so I just trimm the line off until we reach the start of that column.
 '''
 
 import os
 import os.path
-#import 
+import sys
 
 submission_dir = os.path.join(os.getcwd(), 'submissions')
+roster_file = 'roster.dat'
+try:
+    roster_file = sys.argv[1]
+except IndexError:
+    pass
+
 #open roster
-with open('roster.dat', 'r') as roster_file:
+with open(roster_file, 'r') as roster_file:
     for line in roster_file.readlines():
-        sid = line[:8]
-        ucid = line[9 : line.find('@')]
+        sid = line[:8].strip()
+        if '@' in line:
+            ucid = line[9 : line.find('@')].strip()
+        else:
+            ucid = line[9:].strip()
         #print "sid: %s, ucid: %s" % (sid, ucid)
 
         old_filename = os.path.join(submission_dir, sid + '.txt')
